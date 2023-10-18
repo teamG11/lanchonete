2eb8d63f7f535b677c3dddd08aa82d5c91418838
Leonardo Reis <leoeareis@gmail.com>
2023-10-18 19:51:37 -0300

    Altera rotas relacionas ao cliente para termos as rotas de maneira funcional

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import { clienteRouter } from "./adapter/api/routes/ClienteRouter";
 import { produtoRouter } from "./adapter/api/routes/ProdutoRouter";
+import { pedidoRouter } from "./adapter/api/routes/PedidoRouter";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 
 app.use("/cliente", clienteRouter);
 app.use("/produto", produtoRouter);
+app.use("/pedido", pedidoRouter);
 
 export default app;
