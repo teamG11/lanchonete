a424ad6192f3da6b278dc40e5390fff1c961054f
Lucas W. da Motta <lucaswantzg@gmail.com>
2023-10-21 04:36:16 -0300

    Ajuste migration e script para rodar migration no docker

--- prisma/migrations/20231021023128_rename_columns/migration.sql
+++ prisma/migrations/20231021023128_rename_columns/migration.sql
@@ -5,8 +5,8 @@ ALTER TABLE "cliente" RENAME COLUMN "update_at" TO "updated_at";
 ALTER TABLE "pedido" RENAME COLUMN "update_at" TO "updated_at";
 
 -- AlterTable
-ALTER TABLE "pedido_itens" RENAME COLUMN "create_at" TO "created_at",
-RENAME COLUMN "update_at" TO "updated_at";
+ALTER TABLE "pedido_itens" RENAME COLUMN "create_at" TO "created_at";
+ALTER TABLE "pedido_itens" RENAME COLUMN "update_at" TO "updated_at";
 
 -- AlterTable
 ALTER TABLE "produto" RENAME COLUMN "update_at" TO "updated_at";
