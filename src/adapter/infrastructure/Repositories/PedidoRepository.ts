7247f8103e3495cc7d482e1873af0ecfd757fe5e
joaogdcarvalho <27590118+joaogdcarvalho@users.noreply.github.com>
2023-10-22 16:51:57 -0300

    feat(pedido): Implementar PedidoRepository

--- src/adapter/infrastructure/Repositories/PedidoRepository.ts
+++ src/adapter/infrastructure/Repositories/PedidoRepository.ts
@@ -1,14 +1,35 @@
-import { Pedido } from "@/core/domain/Entities/Pedido";
+import { Pedido as PedidoDomain } from "@/core/domain/Entities/Pedido";
 import { IPedidoRepository } from "@/core/domain/Repositories/IPedidoRepository";
+import { prisma } from "@/lib/prisma";
 
 export default class PedidoRepository implements IPedidoRepository {
-    saveAsync(_Pedido: Pedido): Promise<Pedido> {
-        throw new Error("Method not implemented.");
+  async saveAsync(data: PedidoDomain): Promise<PedidoDomain> {
+    const createdPedido = await prisma.pedido.create({ data });
+    return createdPedido;
+  }
+
+  async updateAsync(data: PedidoDomain): Promise<PedidoDomain | null> {
+    const { id, ...pedidoData } = data;
+
+    const existePedido = await prisma.pedido.findUnique({
+      where: { id },
+    });
+
+    if (!existePedido) {
+      throw new Error(`Pedido com ID ${id} não encontrado.`);
     }
-    updateAsync(_Pedido: Pedido): Promise<Pedido> {
-        throw new Error("Method not implemented.");
-    }
-    findByIdAsync(_id: string): Promise<Pedido> {
-        throw new Error("Method not implemented.");
-    }
+
+    const pedidoAtualizado = await prisma.pedido.update({
+      where: { id },
+      data: pedidoData,
+    });
+
+    return pedidoAtualizado;
+  }
+
+  async findByIdAsync(id: number): Promise<PedidoDomain | null> {
+    return prisma.pedido.findUnique({
+      where: { id },
+    });
+  }
 }
